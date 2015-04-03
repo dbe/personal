f370e02ff51fbf0555228eaf461970f9a44bbbd8
Brian Ethier <davidbrianethier@gmail.com>
2015-04-03 15:54:28 -0400

    Taking out twitch_bot link for now. Making products link a stub to under_construction so that I can publish the site before finishing the products section

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'home#static', :defaults => {:first_nav => 'about'}
 
-  get '/:first_nav(/:second_nav(/:third_nav))', :to => 'home#static', :defaults => {:first_nav => 'about', :second_nav => 'career', :third_nav => 'twitch_bot'}
+  get '/:first_nav(/:second_nav(/:third_nav))', :to => 'home#static', :defaults => {:first_nav => 'about', :second_nav => 'career', :third_nav => 'under_construction'}
 end
