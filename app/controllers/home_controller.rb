f7d1d35dfd983649f7267b72c531338fa48c0052
Brian Ethier <davidbrianethier@gmail.com>
2015-04-08 17:03:12 -0400

    Adding contact page and full about page bio

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
   def static
     if params[:first_nav] == 'about'
       return render 'about'
+    elsif params[:first_nav] == 'contact'
+      return render 'contact'
     elsif params[:second_nav] == 'career'
       return render 'career'
     else
