5ab00a90905fc1f385da6d92dc15827e7e0c41d6
Brian Ethier <davidbrianethier@gmail.com>
2016-11-05 15:31:17 -0400

    Refactoring a bit of code in prep for using fish inheritance. Making Fish more correctly object oriented

--- app/assets/javascripts/emerge/application.js
+++ app/assets/javascripts/emerge/application.js
@@ -3,7 +3,7 @@ console.log("In emerge application js");
 var renderer;
 var stage;
 var fish;
-var graphics;
+var velocityVectorGraphics;
 
 setupRenderer();
 setupGraphics();
@@ -15,9 +15,9 @@ function setupRenderer() {
 }
 
 function setupGraphics() {
-  graphics = new PIXI.Graphics();
+  velocityVectorGraphics = new PIXI.Graphics();
 
-  stage.addChild(graphics);
+  stage.addChild(velocityVectorGraphics);
 
   PIXI.loader
     .add("assets/emerge/fish.png")
@@ -36,7 +36,7 @@ function setupGraphics() {
 function animate() {
   requestAnimationFrame(animate);
 
-  graphics.clear();
+  velocityVectorGraphics.clear();
 
   fish.move();
 
@@ -50,53 +50,54 @@ function animate() {
 function Fish() {
   PIXI.Sprite.call(this, PIXI.loader.resources["assets/emerge/fish.png"].texture);
 
-  this.velocity = new Victor(0,0);
-  this.maxSpeed = 5;
-  this.maxForce = 0.1;
+  var velocity = new Victor(0,0);
+  var maxSpeed = 5;
+  var maxForce = 0.1;
+  var calculateDestination = Util.mouseDestination;
+  var that = this;
 
-  //this.acceleration = 0.01;
-
-  this.destination = new PIXI.Point(250,250);
-
+  //-------- Public Functions -------//
 
   Fish.prototype.move = function() {
+    updateVelocity();
 
-    
-    //TODO: Clean this up by getting it out of the class and using a callback to set the destination on this object
-    this.destination = renderer.plugins.interaction.mouse.global;
-
-    var desired = Util.calculateUnitVector(this.position, this.destination);
-    desired.multiplyScalar(this.maxSpeed);
-
-    var steering = desired.clone().subtract(this.velocity);
-
-    //If steering exceeds max force
-    if(steering.magnitude() > this.maxForce) {
-      steering.divideScalar(steering.magnitude() / this.maxForce);
-    }
-
-    this.velocity = steering.clone().add(this.velocity);
-
-    drawVelocities(this.velocity, desired, steering, this.position);
-
-    this.x += this.velocity.x;
-    this.y += this.velocity.y;
+    this.x += velocity.x;
+    this.y += velocity.y;
   }
 
+  //-------- Private Functions ------//
+
+  function updateVelocity() {
+    var destination = calculateDestination();
+
+    var desired = Util.calculateUnitVector(that.position, destination);
+    desired.multiplyScalar(maxSpeed);
+
+    var steering = desired.clone().subtract(velocity);
+
+    //If steering exceeds max force
+    if(steering.magnitude() > maxForce) {
+      steering.divideScalar(steering.magnitude() / maxForce);
+    }
+
+    velocity = steering.clone().add(velocity);
+
+    //TODO: Make that optional based on debug settings
+    drawVelocities(velocity, desired, steering, that.position);
+  }
   
   function drawVelocities(current, desired, steering, origin) {
     drawVelocity(desired, 0x00FF00, origin, 50);
     drawVelocity(current, 0x0000FF, origin, 50);
     drawVelocity(steering, 0xFF0000, origin, 50 * 50);
   }
-  
 
   function drawVelocity(v, color, origin, lineScale) {
-    graphics.beginFill(color)
-    graphics.lineStyle(2, color);
+    velocityVectorGraphics.beginFill(color)
+    velocityVectorGraphics.lineStyle(2, color);
 
-    graphics.moveTo(origin.x, origin.y);
-    graphics.lineTo(origin.x + v.x * lineScale, origin.y + v.y * lineScale);
+    velocityVectorGraphics.moveTo(origin.x, origin.y);
+    velocityVectorGraphics.lineTo(origin.x + v.x * lineScale, origin.y + v.y * lineScale);
   }
 }
 
@@ -113,4 +114,9 @@ Util.calculateUnitVector = function(a, b) {
   vec.normalize();
   
   return vec
+}
+
+//Just returns a PIXI.Point where the mouse is currently
+Util.mouseDestination = function() {
+  return renderer.plugins.interaction.mouse.global;
 }
